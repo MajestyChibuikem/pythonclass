3d02ecb7c61d8e23cf320d5376f5e6aa663f588f
MajestyChibuikem <majestychibuikem@gmail.com>
2025-09-22 13:18:32 +0100

    pip venv

--- assignments/countdown.py
+++ assignments/countdown.py
@@ -14,7 +14,7 @@ This program creates a simple countdown timer that counts from 10 to 1.
 It uses a while loop and the 'time' module for delays.
 """
 
-def countDown():
+def functionToCount():
     print("=== COUNTDOWN TIMER ===")
     print("Starting countdown from 10 to 1...\n")
 
@@ -22,8 +22,24 @@ def countDown():
     count = 10
     while count >= 0:
         print(f" {count}...")
-        time.sleep(1)  # Pause the program for 1 second
-        count -= 1
+        # time.sleep(1)  # Pause the program for 1 second
         count = count -1
 
     print("=== TIMER FINISHED ===")
+
+
+human = {
+    "name": "majesty",
+    "gender": "male"
+}
+
+
+def countup():
+    isFalse = True
+    count = 0
+    while isFalse:
+        print(count)
+        count = count + 1
+        if(count >= 10):
+            isFalse = False
+            break
